3387b1fe1c8c1cf5c7e8989a0c13f3c12858d365
Helen8305 <vsegdaelena@bk.ru>
2022-12-01 17:23:34 +0500

    Initial commit

---
+++ src/test/java/com/example/testthymrleafwebapp/TestThymrleafWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testthymrleafwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestThymrleafWebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
